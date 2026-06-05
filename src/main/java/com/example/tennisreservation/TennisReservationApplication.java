95383b5ccb797c7a5a6c743e374f18a4df37a00f
xdvora20 <xdvora20@fi.muni.cz>
2026-06-05 15:46:02 +0200

    chore: Project setup

---
+++ src/main/java/com/example/tennisreservation/TennisReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennisreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tennisreservation/TennisReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennisreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
